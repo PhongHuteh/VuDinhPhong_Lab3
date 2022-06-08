4fe9824d45adf2e62b8aa5e07b1fbbc124756a5b
Vũ Đình Phong <vudinhphong.261001@gmail.com>
2022-06-08 12:16:49 +0700

    Add project files.

---
+++ VuDinhPhong_Lab3/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VuDinhPhong_Lab3.Models;
+using System.Data.Entity;
+namespace VuDinhPhong_Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        private ApplicationDbContext _dbContext;
+
+        public ActionResult Index()
+        {
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcommingCourses);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ VuDinhPhong_Lab3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VuDinhPhong_Lab3.Startup))]
+namespace VuDinhPhong_Lab3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
